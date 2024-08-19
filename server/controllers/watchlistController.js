c9402e318f2ca2f5230dd54869388651216237ec
rtfmanual <72383992+rtfmanual@users.noreply.github.com>
2024-08-19 20:08:07 +0200

    Added CRD endpoints!

--- server/controllers/watchlistController.js
+++ server/controllers/watchlistController.js
@@ -77,3 +77,62 @@ exports.startAllServices = async () => {
     console.error('Error starting services', err.message);
   }
 };
+
+exports.createWatchlist = (req, res) => {
+  const { name, category, sizes, brands, maxPrice, gender, url } = req.body;
+  const watchlistDocument = new Watchlist({
+    name,
+    category,
+    sizes,
+    brands,
+    maxPrice,
+    gender,
+    url,
+  });
+
+  watchlistDocument.save()
+    .then(savedWatchlist => {
+      res.status(201).json({
+        message: 'Watchlist created successfully',
+        watchlist: savedWatchlist
+      });
+    })
+    .catch(error => {
+      console.error('Error saving watchlist:', error);
+      if (error.name === 'ValidationError') {
+        res.status(400).json({
+          message: 'Validation error',
+          error: error.message
+        });
+      } else {
+        res.status(500).json({
+          message: 'An error occurred while creating the watchlist',
+          error: error.message
+        });
+      }
+    });
+};
+
+exports.getWatchlists = async () => {
+  try {
+    const watchlists = await Watchlist.find();
+    return watchlists;
+  } catch (error) {
+    throw new Error('Error getting watchlists: ' + error.message);
+  }
+};
+
+exports.deleteWatchlist = async (watchlistId) => {
+  try {
+    const deletedWatchlist = await Watchlist.findByIdAndDelete(watchlistId);
+
+    if (!deletedWatchlist) {
+      throw new Error('Watchlist not found');
+    }
+
+    return deletedWatchlist;
+  } catch (error) {
+    console.error('Error deleting watchlist:', error.message);
+    throw error;
+  }
+};
--- server/routes/brandRoutes.js
+++ server/routes/brandRoutes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const brandController = require('../controllers/brandController');
 
+router.get('/getAll', async (req, res) => {
+  const brands = await brandController.getBrands()
+  if (brands) {
+    res.json({ brands })
+  }
+  else {
+    res.status(500)
+  }
+})
+
 // Example route if you need to expose the getBrandCode function
 router.get('/code/:brandName', async (req, res) => {
   const brandCode = await brandController.getBrandCode(req.params.brandName);
