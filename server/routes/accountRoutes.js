ba6cb2df8ddfaba28b5275f2361947f9aed44d40
rtfmanual <xvlaze@gmail.com>
2024-12-18 10:09:47 +0100

    Cleaned the endpoints a little bit

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dockerController = require('../controllers/dockerController');
 
-router.get('/startAllHellasteeze', (req, res) => {
-  dockerController.startAllHellasteeze();
-});
-
 router.get('/updateCookies', () => {
   dockerController.runCookieMonster();
 })
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const dockerController = require('../controllers/dockerController');
 
 router.get('/list', productController.getAllProducts);
+router.get('/update', dockerController.startAllHellasteeze());
 router.put('/watch', productController.watch)
 router.delete('/unwatch', productController.unwatch)
 
